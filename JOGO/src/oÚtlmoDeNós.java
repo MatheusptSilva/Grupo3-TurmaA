05ec0b1a60e44e3068c7318e3dace0b37dab39a6
Guilherme Alves Martins <guilhermealves.1999@hotmail.com>
2021-05-15 00:05:58 -0300

    Update oÚtlmoDeNós.java
    Introdução do Jogo

--- JOGO/src/oÚtlmoDeNós.java
+++ JOGO/src/oÚtlmoDeNós.java
@@ -1,69 +1,44 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.Collections;
+import java.util.ArrayList;
+public class Jogo {
 
-public class oÚtlmoDeNós 
-
-{
-
-	public static void main(String[] args)
-	
-	{
-		
+	static int temp_dialog =100, temp_narrativa = 100, temp_transicao = 100;
+	public static void main(String[] args) throws Exception {
 		Scanner entrada = new Scanner (System.in);
 		
-		boolean menu = false;
-		String nome;
-        int opcao; 
+		Escreve("\n *-----------* Seja Bem Vindo ao Jogo *-----------*", TimeUnit.MILLISECONDS, temp_dialog);	
+		Escreve("\n                 O ÚLTIMO DE NÓS                   ", TimeUnit.MILLISECONDS, temp_dialog);
+		System.out.println("\n   ");
+		Escreve("\n Você está preparado para encarar o futuro... meu caro jogador?", TimeUnit.MILLISECONDS, temp_dialog);
+		System.out.println("\n ");
+		System.out.println("\n 1|SIM, estou pronto!       2|Talvez, não sei o que me espera...           3|Não, tenho medo!!!");
+		int responder = entrada.nextInt();
+		if(responder == 1) {
+			Escreve("Muito bem... Muito bem... Vejamos só, temos um corajoso por aqui! A grande matrix amará enfrentá-lo HAHAHAH. Vejamos se é capaz de vencer um computador!!!!", TimeUnit.MICROSECONDS, temp_dialog);
+			menu();
+		} else if(responder == 2) {
+			Escreve("Ora, ora... Achei que era mais corajoso, pobre criatura... Está ai, um grande motivo pelo qual os computadores são bem mais desenvolvidos e ultrapassaram a raça humana!", TimeUnit.MILLISECONDS, temp_dialog);
+			menu();
+		}else if(responder ==3) {
+			Escreve("Vejamos só o que temos por aqui: o pior da peste humana! Chorará e lamentará no reinado das máquinas, já está aqui verme... PROSSIGA!", TimeUnit.MICROSECONDS, temp_dialog);
+			menu();
+		}
 		
-		System.out.println(" *-----------* Seja Bem Vindo ao Jogo *-----------* ");
-		System.out.println("\n                  O ÚLTIMO DE NÓS                   ");
+
+	}
+	public static void Escreve(String mensagem, TimeUnit unit, long tempo_mensagem) throws InterruptedException {
+        for (char caractere : mensagem.toCharArray()) {
+        System.out.print(caractere);
+        unit.sleep(tempo_mensagem);
+    }
+}
+	
+
 		
-		System.out.print("\nPor favor, digite o nome do player: ");
-		nome = entrada.next();
-		
-		System.out.printf("\nOlá %s", nome);
-		
-		do {
-			
-			System.out.println("\nDigite a opção desejada: ");
-			
-			
-			System.out.println("\n(1) - Iniciar");
-			System.out.println("(2) - Instruções");
-			System.out.println("(3) - Como Jogar");
-			System.out.println("(4) - Sair");
-			
-			opcao = entrada.nextInt();
-			
-			switch(opcao) {
-			case 1:
-				menu = true;
-			break;
-			case 2:
-				System.out.println(nome + " as instruções são básicas, você deve ajudar Will Smith a desvendar todos os mistérios de CONCEITOS DA COMPUTAÇÃO. \r\n"
-						+"\r\n"
-						+ "Somente assim, will será capaz de vencer todas essas máquinas que estão ansiosas para lhe derrotar!!!!\r\n"
-						+ "\r\n"
-						+ "Lembre-se dos números e de suas conversões de bases. Este será o passo mais fácil que você percorrerá. A dificuldade aumentará ao avançar.\r\n"
-						+ "\r\n"
-						+ "Espero que se lembre de Maurice Karnaugh. Afinal, o mapa que ele aprimorou será um dos maiores desafios a ser enfrentados.\r\n"
-						+ "\r\n"
-						+ "Agora siga! Avante!!!! \\r\\n");
-						
-				break;
-			case 3:
-				System.out.println("No jogo todas as perguntas são de alternativas, por tanto leia atentamente as perguntas e digite a reposta que desejar, porém"
-						+ " "
-						+ "tome cuidado, pois você terá chances limitadas. ");
-				break;
-			case 4:
-				System.out.println("");
-				break;
-				default:
-					System.out.println("Digite uma opção válida");
-			}
-			
-		}while(menu==false);
+	
+
+	static void menu() {
 		
 	}
-	
-}
