ec5d80782634a198caeb221bdd35fadeed86cbf2
Guilherme Alves Martins <guilhermealves.1999@hotmail.com>
2021-05-16 12:38:23 -0300

    Update Jogo.java

--- RPG/src/Jogo.java
+++ RPG/src/Jogo.java
@@ -1,47 +1,77 @@
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.Collections;
+import java.util.ArrayList;
 
 public class Jogo {
 
-	static int temp_dialog =100, temp_narrativa = 100, temp_transicao = 100;
+	static int temp_dialog = 100, temp_narrativa = 100, temp_transicao = 100;
+
 	public static void main(String[] args) throws Exception {
-		Scanner entrada = new Scanner (System.in);
-		
-		Escreve("\n *-----------* Seja Bem Vindo ao Jogo *-----------*", TimeUnit.MILLISECONDS, temp_dialog);	
+		Scanner entrada = new Scanner(System.in);
+
+		Escreve("\n *-----------* Seja Bem Vindo ao Jogo *-----------*", TimeUnit.MILLISECONDS, temp_dialog);
 		Escreve("\n                 O ÚLTIMO DE NÓS                   ", TimeUnit.MILLISECONDS, temp_dialog);
 		System.out.println("\n   ");
-		Escreve("\n Você está preparado para encarar o futuro... meu caro jogador?", TimeUnit.MILLISECONDS, temp_dialog);
+		Escreve("\n Você está preparado para encarar o futuro... meu caro jogador?", TimeUnit.MILLISECONDS,
+				temp_dialog);
 		System.out.println("\n ");
-		System.out.println("\n 1|SIM, estou pronto!       2|Talvez, não sei o que me espera...           3|Não, tenho medo!!!");
+		System.out.println(
+				"\n 1|SIM, estou pronto!       2|Talvez, não sei o que me espera...           3|Não, tenho medo!!!");
 		int responder = entrada.nextInt();
-		if(responder == 1) {
-			Escreve("Muito bem... Muito bem... Vejamos só, temos um corajoso por aqui! A grande matrix amará enfrentá-lo HAHAHAH. Vejamos se é capaz de vencer um computador!!!!", TimeUnit.MILLISECONDS, temp_dialog);
+		if (responder == 1) {
+			Escreve("Muito bem... Muito bem... Vejamos só, temos um corajoso por aqui! A grande matrix amará enfrentá-lo HAHAHAH. Vejamos se é capaz de vencer um computador!!!!",
+					TimeUnit.MILLISECONDS, temp_dialog);
 			menu();
-		} else if(responder == 2) {
-			Escreve("Ora, ora... Achei que era mais corajoso, pobre criatura... Está ai, um grande motivo pelo qual os computadores são bem mais desenvolvidos e ultrapassaram a raça humana!", TimeUnit.MILLISECONDS, temp_dialog);
+		} else if (responder == 2) {
+			Escreve("Ora, ora... Achei que era mais corajoso, pobre criatura... Está ai, um grande motivo pelo qual os computadores são bem mais desenvolvidos e ultrapassaram a raça humana!",
+					TimeUnit.MILLISECONDS, temp_dialog);
 			menu();
-		}else if(responder ==3) {
-			Escreve("Vejamos só o que temos por aqui: o pior da peste humana! Chorará e lamentará no reinado das máquinas, já está aqui verme... PROSSIGA!", TimeUnit.MILLISECONDS, temp_dialog);
+		} else if (responder == 3) {
+			Escreve("Vejamos só o que temos por aqui: o pior da peste humana! Chorará e lamentará no reinado das máquinas, já está aqui verme... PROSSIGA!",
+					TimeUnit.MILLISECONDS, temp_dialog);
 			menu();
 		}
-		
 
 	}
+
 	public static void Escreve(String mensagem, TimeUnit unit, long tempo_mensagem) throws InterruptedException {
-        for (char caractere : mensagem.toCharArray()) {
-        System.out.print(caractere);
-        unit.sleep(tempo_mensagem);
-    }
-}
-	
+		for (char caractere : mensagem.toCharArray()) {
+			System.out.print(caractere);
+			unit.sleep(tempo_mensagem);
+		}
+	}
 
-		
-	
+	static void menu() throws Exception {
+		Scanner entrada = new Scanner(System.in);
+		int opcao_menu;
+		do {
+			Escreve("\n\n..::×  O ÚLTIMO DE NÓS  ×::.. \n\n1 - Jogar \n2 - Sobre o Jogo\n3 - Créditos \n4 - Sair",
+					TimeUnit.MILLISECONDS, temp_transicao);
+			System.out.println("\n");
+			opcao_menu = entrada.nextInt();
+			if (opcao_menu == 1) {
+				jogar();
+			} else if (opcao_menu == 2) {
+				sobre();
+			} else if (opcao_menu == 3) {
+				creditos();
+			} else if (opcao_menu == 4) {
+				System.exit(0);
+			} else {
+				System.out.println("\nOpção inválida, digite novamente: ");
+			}
+		} while (opcao_menu != 1 && opcao_menu != 2 && opcao_menu != 3 && opcao_menu != 4);
+	}
 
-	 static void menu() throws Exception {
-		    Scanner entrada = new Scanner (System.in);
-		    int escolha_menu;
-		    
-	 }
+	static void jogar() throws Exception {
 
+	}
+	static void sobre() throws Exception {
+
+	}
+
+	static void creditos() throws Exception {
+
+	}
 }
