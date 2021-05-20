b1453ebdaa8f3d3f0f860a4fdc7ffd4702afa358
Gustavo6401 <gsoey2019123@gmail.com>
2021-05-20 14:18:17 -0300

    Seis perguntas já feitas por mim

--- RPG/src/Jogo.java
+++ RPG/src/Jogo.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Jogo {
 
 	static int temp_dialog = 100, temp_narrativa = 100, temp_transicao = 100;
+	static int _acerto = 0;
 
 	public static void main(String[] args) throws Exception {
 		Scanner entrada = new Scanner(System.in);
@@ -65,8 +66,166 @@ public class Jogo {
 	}
 
 	static void jogar() throws Exception {
+		Scanner entrada = new Scanner(System.in);
 
+        int acerto = 0;
+
+        // Pergunta 1
+        System.out.println("*--Pergunta 1--*");        
+        System.out.println("45 em binário?");
+        System.out.println("A - 101101");
+        System.out.println("B - 010011");
+        System.out.println("C - 111111");
+        System.out.println("D - 000011");
+        System.out.println("E - 110011");
+        String resposta1 = entrada.nextLine();
+
+        resposta1 = resposta1.toUpperCase();
+        
+        switch(resposta1) {
+            case "A":
+                System.out.println("Você acertou!");
+
+                break;
+            case "B":
+            case "C":
+            case "D":
+            case "E":
+                System.out.println("Você errou!");
+            default:
+                System.out.println("Você errou!");
+        }
+
+        // Pergunta 2
+        System.out.println("*--Pergunta 2--*");        
+        System.out.println("10101 em Decimal");
+        System.out.println("A - 24");
+        System.out.println("B - 99");
+        System.out.println("C - 25");
+        System.out.println("D - 21");
+        System.out.println("E - 15");
+        String resposta2 = entrada.nextLine();
+
+        resposta2 = resposta2.toUpperCase();
+        
+        switch(resposta2) {
+            case "D":
+                System.out.println("Você acertou!");
+
+                break;
+            case "B":
+            case "C":
+            case "A":
+            case "E":
+                System.out.println("Você errou!");
+            default:
+                System.out.println("Você errou!");
+        }
+
+        // Pergunta 3        
+        System.out.println("*--Pergunta 3--*");        
+        System.out.println("58 em binário?");
+        System.out.println("A - 011111");
+        System.out.println("B - 111011");
+        System.out.println("C - 111010");
+        System.out.println("D - 111001");
+        System.out.println("E - 100000");
+        String resposta3 = entrada.nextLine();
+
+        resposta3 = resposta3.toUpperCase();
+        
+        switch(resposta3) {
+            case "C":
+                System.out.println("Você acertou!");
+
+                break;
+            case "B":
+            case "A":
+            case "D":
+            case "E":
+                System.out.println("Você errou!");
+            default:
+                System.out.println("Você errou!");
+        }
+
+        // Pergunta 4
+        System.out.println("*--Pergunta 4--*");        
+        System.out.println("AF5 em decimal?");
+        System.out.println("A - 5.365");
+        System.out.println("B - 5.000");
+        System.out.println("C - 300");
+        System.out.println("D - 6.000");
+        System.out.println("E - 5.360");
+        String resposta4 = entrada.nextLine();
+
+        resposta4 = resposta4.toUpperCase();
+        
+        switch(resposta4) {
+            case "A":
+                System.out.println("Você acertou!");
+
+                break;
+            case "B":
+            case "C":
+            case "D":
+            case "E":
+                System.out.println("Você errou!");
+            default:
+                System.out.println("Você errou!");
+        }
+
+        System.out.println("*--Pergunta 4--*");        
+        System.out.println("F17 em octal?");
+        System.out.println("A - 7.00");
+        System.out.println("B - 7.424");
+        System.out.println("C - 7.017");
+        System.out.println("D - 7.427");
+        System.out.println("E - 7.217");
+        String resposta5 = entrada.nextLine();
+
+        resposta5 = resposta5.toUpperCase();
+        
+        switch(resposta5) {
+            case "D":
+                System.out.println("Você acertou!");
+
+                break;
+            case "B":
+            case "C":
+            case "A":
+            case "E":
+                System.out.println("Você errou!");
+            default:
+                System.out.println("Você errou!");
+        }
+
+        // Pergunta 6
+        System.out.println("*--Pergunta 4--*");        
+        System.out.println("(octal) 7.000 em decimal?");
+        System.out.println("A - 3.584");
+        System.out.println("B - 3.583");
+        System.out.println("C - 3.585");
+        System.out.println("D - 3.000");
+        System.out.println("E - 3.586");
+        String resposta6 = entrada.nextLine();
+
+        resposta6 = resposta6.toUpperCase();
+        
+        switch(resposta6) {
+            case "A":
+                System.out.println("Você acertou!");
+
+                break;
+            case "B":
+            case "C":
+            case "D":
+            case "E":
+                System.out.println("Você errou!");
+            default:
+                System.out.println("Você errou!");
+        }
 	}
+	
 	static void sobre() throws Exception {
 
 	}
@@ -76,7 +235,7 @@ public class Jogo {
         int opcao_menu, temp_narrativa = 150;
         System.out.println("SENAC - SERVIÇO NACIONAL DE APRENDIZAGEM COMERCIAL \nJogo elaborado para o PROJETO INTEGRADOR para a matéria de PI, no curso de ANÁLISE E DESENVOLVIMENTO DE SISTEMAS! \nParticiparam ativamente da criação do jogo rpg textual(desenvolvedores):");
         
-        Escreve("\n\n-> •Guilherme Alves \n-> •Vinícius Santana \n-> •Matheus Thome \n-> •Gustavo Souza",TimeUnit.MILLISECONDS, temp_narrativa);
+        Escreve("\n\n-> •Guilherme Alves \n-> •Vinícius Santana \n-> •Matheus Thome \n-> •Gustavo Souza \n-> •Gustavo da Silva Oliveira",TimeUnit.MILLISECONDS, temp_narrativa);
         do{
         System.out.println("\n2 - Sair");
         opcao_menu = entrada.nextInt();
